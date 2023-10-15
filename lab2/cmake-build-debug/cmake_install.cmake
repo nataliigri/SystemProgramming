0b3939b94f19a21f55c198f5d91b3d3d8532a564
Наталя Грицишин <nataliiagricisin@Natalas-Air.netis>
2023-10-16 01:27:37 +0300

    first commit

--- lab2/cmake-build-debug/cmake_install.cmake
+++ lab2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/nataliiagricisin/Desktop/System Programming/lab2
+# Install script for directory: /Users/nataliiagricisin/Desktop/System Programming/lab2cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/nataliiagricisin/Desktop/System Programming/lab2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/nataliiagricisin/Desktop/System Programming/lab2cpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
